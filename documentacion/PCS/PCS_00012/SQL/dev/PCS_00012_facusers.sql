7047a0f28f76c872617bea73c5eb9d00eaa1dca9
Ángel Luis Berasuain Ruiz <angel.berasuain@gmail.com>
2017-06-13 14:31:31 +0200

    Documentación de asignación de datos de la zona pública

--- documentacion/PCS/PCS_00012/SQL/dev/PCS_00012_facusers.sql
+++ documentacion/PCS/PCS_00012/SQL/dev/PCS_00012_facusers.sql
@@ -38,6 +38,59 @@ ALTER TABLE `inmuebles_enlaces` ADD `youtube` TINYINT(1) NOT NULL DEFAULT '0' ;
 
 
 
+/* -- Dependientes del idioma
+
+Título
+
+URL SEO -> autocalculada
+
+Descripción SEO -> descripción corta
+
+Descripcion vivienda (HTML)
+
+keywords
+
+-- No dependientes del idioma
+
+Publicado
+
+Oportunidad
+
+Destacado
+
+*/
+
+--
+-- Estructura de tabla para la tabla `inmuebles_idiomas`
+--
+
+CREATE TABLE IF NOT EXISTS `inmuebles_idiomas` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(70) NOT NULL,
+  `descripcion` text NOT NULL,
+  `url_seo` varchar(70) NOT NULL,
+  `descripcion_seo` varchar(150) NOT NULL,
+  `keywords_seo` varchar(255) NOT NULL,
+  `inmueble_id` int(11) unsigned NOT NULL,
+  `idioma_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_inmuebles_idiomas_titulo` (`titulo`),
+  UNIQUE KEY `unique_inmuebles_idiomas_url_seo` (`url_seo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
+CREATE INDEX FK_inmuebles_idiomas_inmueble_id ON inmuebles_idiomas (inmueble_id);
+ALTER TABLE `inmuebles_idiomas` ADD CONSTRAINT `FK_inmuebles_idiomas_inmueble_id` FOREIGN KEY (`inmueble_id`) REFERENCES `inmuebles` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
 
+CREATE INDEX FK_inmuebles_idiomas_idioma_id ON inmuebles_idiomas (idioma_id);
+ALTER TABLE `inmuebles_idiomas` ADD CONSTRAINT `FK_inmuebles_idiomas_idioma_id` FOREIGN KEY (`idioma_id`) REFERENCES `idiomas` (`id_idioma`) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+ALTER TABLE `inmuebles` CHANGE `publicado` `publicado` TINYINT(1) NOT NULL DEFAULT '0';
+ALTER TABLE `inmuebles` ADD `oportunidad` TINYINT(1) NOT NULL DEFAULT '0';
+ALTER TABLE `inmuebles` ADD `destacado` TINYINT(1) NOT NULL DEFAULT '0';
+
+ALTER TABLE `inmuebles` CHANGE `direccion_publica` `direccion_publica` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `inmuebles` CHANGE `direccion` `direccion` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+ALTER TABLE `seccion_idiomas` CHANGE `titulo` `titulo` VARCHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
