943a66f383ae8faaeaa8eddbcf5cbb913ea898d3
Ángel Luis Berasuain Ruiz <angel.berasuain@gmail.com>
2017-07-27 11:16:39 +0200

    Eliminación de index.php para configuraciones de entornos y soporte htaccess

--- openrs/config/production/config.php
+++ openrs/config/production/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://www.produccion.es/demo/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
