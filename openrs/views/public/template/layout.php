5e5788c3a0b484d0550fae23b12b98a6384a703d
Ángel Luis Berasuain Ruiz <angel.berasuain@gmail.com>
2017-08-08 19:20:48 +0200

    Usamos login de zona pública con materialize así no hay que cambiarlo

--- openrs/views/public/template/layout.php
+++ openrs/views/public/template/layout.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-        <title>Starter Template - Materialize</title>
+        <title>OPENRS - Login</title>
 
         <!-- CSS  -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -16,14 +16,18 @@
         <script src="<?php echo base_url(); ?>assets/public/js/init.js"></script>
     </head>
     <body>
+        
         <nav class="light-blue lighten-1" role="navigation">
-            <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+            <div class="nav-wrapper container">
+                <a id="logo-container" href="#" class="brand-logo">OPENRS</a>
                 <ul class="right hide-on-med-and-down">
-                    <li><a href="#">Navbar Link</a></li>
+                    <li><a href="http://www.openrs.es/documentacion/codigo/html/">Documentación</a></li>
+                    <li><a href="https://github.com/klaimir/openrs">Repositorio</a></li>
                 </ul>
 
                 <ul id="nav-mobile" class="side-nav">
-                    <li><a href="#">Navbar Link</a></li>
+                    <li><a href="http://www.openrs.es/documentacion/codigo/html/">Documentación</a></li>
+                    <li><a href="https://github.com/klaimir/openrs">Repositorio</a></li>
                 </ul>
                 <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
             </div>
@@ -56,10 +60,12 @@
         <footer class="page-footer orange">
             <div class="container">
                 <div class="row">
-                    <div class="col l7 s12">
-                        <h5 class="white-text">Company Bio</h5>
-                        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+                    <div class="col s12">
+                        <h5 class="white-text">Bienvenido a OPENRS¡</h5>
+                        <p class="grey-text text-lighten-4">Empieza a disfrutar de las ventajas de usar este software pensado para PYMES inmobiliarias españolas y 
+                            define tu web corporativa así como todos los datos necesarios para la gestión de tu negocio.</p>
                     </div>
+                    <?php /*
                     <div class="col l2 s12">
                         <h5 class="white-text">Conócenos</h5>
                         <ul>
@@ -72,6 +78,9 @@
                         <p class="white-text">de 9 a 14 y 17 a 20</p>
                         <a class="white-text" href="#!">Facebook</a>
                     </div>
+                     * 
+                     */
+                    ?>
                 </div>
             </div>
         </footer>  
