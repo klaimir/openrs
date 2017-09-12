34290be0847fa8f05c6175633ca298bc7332472c
Ángel Luis Berasuain Ruiz <angel.berasuain@gmail.com>
2017-09-12 11:11:04 +0200

    Corrección de errores de maquetación e instrucciones

--- documentacion/instalacion/adaptaciones.sql
+++ documentacion/instalacion/adaptaciones.sql
@@ -1,7 +1,7 @@
 -- IMPORTANTE: este fichero de adaptación se publica con la preinstalación inicial del software.
--- Si usted está realizando una migración de dominio, en lugar de openrs.es, deberá de indicar el dominio origen desde donde está migrando. (por ejemplo, dominioanterior.es)
+-- Si usted está realizando una migración de dominio, en lugar de openrs.es/demo, deberá de indicar el dominio y carpeta origen desde donde está migrando. (por ejemplo, dominioanterior.es)
 
-UPDATE `footer_texto_idiomas` SET `contenido` =  REPLACE(contenido, 'openrs.es', 'dominioactual.es');
-UPDATE `texto_idiomas` SET `contenido` =  REPLACE(contenido, 'openrs.es', 'dominioactual.es');
-UPDATE `articulos_idiomas` SET `contenido` =  REPLACE(contenido, 'openrs.es', 'dominioactual.es');
-UPDATE `inmuebles_idiomas` SET `contenido` =  REPLACE(contenido, 'openrs.es', 'dominioactual.es');
+UPDATE `footer_texto_idiomas` SET `contenido` =  REPLACE(contenido, 'openrs.es/demo', 'dominioactual.es');
+UPDATE `texto_idiomas` SET `contenido` =  REPLACE(contenido, 'openrs.es/demo', 'dominioactual.es');
+UPDATE `articulos_idiomas` SET `contenido` =  REPLACE(contenido, 'openrs.es/demo', 'dominioactual.es');
+UPDATE `inmuebles_idiomas` SET `contenido` =  REPLACE(descripcion, 'openrs.es/demo', 'dominioactual.es');
